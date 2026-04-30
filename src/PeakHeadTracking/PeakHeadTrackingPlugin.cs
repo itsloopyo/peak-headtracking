f263a3383ced33d684e28bb331e0390b207d9920
Loop <252084443+itsloopyo@users.noreply.github.com>
2026-05-01 00:00:10 +0100

    Release v1.1.0

--- src/PeakHeadTracking/PeakHeadTrackingPlugin.cs
+++ src/PeakHeadTracking/PeakHeadTrackingPlugin.cs
@@ -21,7 +21,7 @@ namespace PeakHeadTracking
         // Plugin metadata constants
         public const string PLUGIN_GUID = "com.cameraunlock.peak.headtracking";
         public const string PLUGIN_NAME = "Peak Head Tracking";
-        public const string PLUGIN_VERSION = "1.0.2";
+        public const string PLUGIN_VERSION = "1.1.0";
 
         // Static logger instance for global access
         internal static new ManualLogSource Logger;
